3e35f9708aad6798ed3d053623c16ece8c802bf7
Abdul Owhab <owhab.cse@gmail.com>
2022-02-13 06:15:38 +0600

    Added Single Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
     </div>
   );
 }
