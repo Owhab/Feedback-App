2f183bac0228e748513b071d5a1d8a99b2a66129
Abdul Owhab <owhab.cse@gmail.com>
2022-02-14 04:01:24 +0600

    Added React Router Dom and Created About Page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from "uuid";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
@@ -21,13 +22,29 @@ function App() {
   };
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
 
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                  <Link to="about">About this projects</Link>
+                </>
+              }
+            />
+            <Route path="about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
